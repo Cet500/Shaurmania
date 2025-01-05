a086bfbd7dffb5e07e2e9615c69a258bfd6b5870
alex2286mk <alt140511@gmail.com>
2025-01-05 17:19:52 +1000

    Working with the database

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from.models import Review
 
-# Register your models here.
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display=( 'id', 'name', 'text', 'stars','date' )
--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator
+
+class Review(models.Model):
+    name = models.CharField(max_length=60)
+    
+    text = models.TextField(max_length=600)
+
+    stars = models.SmallIntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
+
+    date = models.CharField(max_length=30)
 
 # Create your models here.
