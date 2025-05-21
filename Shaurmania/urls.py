1d7879a109962da1eab773d70474754ab965ed20
Cet500 <codeoon@mail.ru>
2025-05-21 19:01:33 +1000

    Created base html-errors handlers

--- Shaurmania/urls.py
+++ Shaurmania/urls.py
@@ -6,11 +6,22 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
+from main import views as main_views
 
 urlpatterns = [
     path( 'admin/', admin.site.urls ),
-    path( '', include( 'main.urls' ) )
+    path( '', include( 'main.urls' ) ),
+
+    path( 'errors/400/', main_views.error_400, {'exception': Exception('Bad Request')},       name = 'errors_400' ),
+    path( 'errors/403/', main_views.error_403, {'exception': Exception('Permission Denied')}, name = 'errors_403' ),
+    path( 'errors/404/', main_views.error_404, {'exception': Exception('Page not Found')},    name = 'errors_404' ),
+    path( 'errors/500/', main_views.error_500, name = 'errors_500' )
 ]
+
+handler400 = 'main.views.error_400'
+handler403 = 'main.views.error_403'
+handler404 = 'main.views.error_404'
+handler500 = 'main.views.error_500'
 
 if settings.DEBUG:
     urlpatterns += static( settings.MEDIA_URL, document_root = settings.MEDIA_ROOT )
--- main/views.py
+++ main/views.py
@@ -134,3 +134,15 @@ def login(request):
 def logout( request ):
     logout_django(request)
     return redirect('index')
+
+def error_400(request, exception):
+    return render(request, 'main/error/400.html', status = 400)
+
+def error_403(request, exception):
+    return render(request, 'main/error/403.html', status = 403)
+
+def error_404(request, exception):
+    return render(request, 'main/error/404.html', status = 404)
+
+def error_500(request):
+    return render(request, 'main/error/500.html', status = 500)
