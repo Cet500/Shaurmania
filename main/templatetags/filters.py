39004b29e5c082e48573a0660fa45e6f60d9bb09
Cet500 <codeoon@mail.ru>
2025-10-20 12:24:21 +1000

    Added 24 new avatars images

--- main/templatetags/filters.py
+++ main/templatetags/filters.py
@@ -5,5 +5,5 @@ register = template.Library()
 
 @register.filter(name='random_image_url')
 def random_image_url(value):
-    numbers = ['{:03d}'.format(i) for i in range(0,33)]
+    numbers = ['{:03d}'.format(i) for i in range(0,57)]
     return '{}.png'.format(random.choice(numbers))
