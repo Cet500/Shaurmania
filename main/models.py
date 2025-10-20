fbcddd52188c659e689a079e68d9bce965d9204a
Cet500 <codeoon@mail.ru>
2025-10-20 12:42:34 +1000

    Renamed models as m ( create alias )

--- main/models.py
+++ main/models.py
@@ -1,19 +1,19 @@
 import uuid
-from django.db import models
+from django.db import models as m
 from django.core.validators import MinValueValidator,MaxValueValidator
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.utils import timezone
 from slugify import slugify
 
 
-class Review( models.Model ):
-    name    = models.CharField( max_length = 60,  verbose_name = 'Имя' )
-    text    = models.TextField( max_length = 600, verbose_name = 'Текст отзыва' )
-    stars   = models.SmallIntegerField( validators = [ MinValueValidator(1), MaxValueValidator(5) ],
+class Review( m.Model ):
+    name    = m.CharField( max_length = 60,  verbose_name = 'Имя' )
+    text    = m.TextField( max_length = 600, verbose_name = 'Текст отзыва' )
+    stars   = m.SmallIntegerField( validators = [ MinValueValidator(1), MaxValueValidator(5) ],
                                         verbose_name = 'Оценка' )
-    shaurma = models.ForeignKey( 'Shaurma', on_delete = models.SET_NULL,
+    shaurma = m.ForeignKey( 'Shaurma', on_delete = m.SET_NULL,
                                  null = True, blank = True, verbose_name = 'Шаурма' )
-    date    = models.DateTimeField( auto_now_add = True, verbose_name = 'Время записи' )
+    date    = m.DateTimeField( auto_now_add = True, verbose_name = 'Время записи' )
 
     def __str__(self):
         return f'Отзыв от {self.name} ( {self.stars} / 5 )'
@@ -24,24 +24,24 @@ class Review( models.Model ):
         ordering = [ 'name' ]
 
 
-class Shaurma( models.Model ):
-    name          = models.CharField( max_length = 60, unique = True, verbose_name = 'Название' )
-    slug          = models.SlugField( max_length = 70, blank = True, verbose_name = "URL-адрес" )
-    category      = models.ForeignKey( 'ShaurmaCategory', on_delete = models.SET_NULL,
+class Shaurma( m.Model ):
+    name          = m.CharField( max_length = 60, unique = True, verbose_name = 'Название' )
+    slug          = m.SlugField( max_length = 70, blank = True, verbose_name = "URL-адрес" )
+    category      = m.ForeignKey( 'ShaurmaCategory', on_delete = m.SET_NULL,
                                        null = True, blank = True, verbose_name = 'Категория' )
-    compound      = models.TextField( max_length = 600, verbose_name = 'Состав' )
-    short_text    = models.TextField( max_length = 200, blank = True, verbose_name = 'Краткое описание' )
-    description   = models.TextField( max_length = 1000, blank = True, verbose_name = 'Описание' )
-    picture       = models.ImageField( upload_to = 'shaurma_images', verbose_name = 'Изображение' )
-    price         = models.PositiveSmallIntegerField( verbose_name = 'Цена в ₽' )
-    weight        = models.PositiveSmallIntegerField( verbose_name = 'Вес в гр' )
-    calories      = models.PositiveIntegerField( default = 0, verbose_name = "Калории (ккал)" )
-    proteins      = models.FloatField( default = 0, verbose_name = "Белки (г)" )
-    fats          = models.FloatField( default = 0, verbose_name = "Жиры (г)" )
-    carbohydrates = models.FloatField( default = 0, verbose_name = "Углеводы (г)" )
-    is_available  = models.BooleanField( default = True, verbose_name = "Доступна для заказа" )
-    created_at    = models.DateTimeField( auto_now_add = True, verbose_name = "Дата создания" )
-    updated_at    = models.DateTimeField( auto_now = True, verbose_name = "Дата обновления" )
+    compound      = m.TextField( max_length = 600, verbose_name = 'Состав' )
+    short_text    = m.TextField( max_length = 200, blank = True, verbose_name = 'Краткое описание' )
+    description   = m.TextField( max_length = 1000, blank = True, verbose_name = 'Описание' )
+    picture       = m.ImageField( upload_to = 'shaurma_images', verbose_name = 'Изображение' )
+    price         = m.PositiveSmallIntegerField( verbose_name = 'Цена в ₽' )
+    weight        = m.PositiveSmallIntegerField( verbose_name = 'Вес в гр' )
+    calories      = m.PositiveIntegerField( default = 0, verbose_name = "Калории (ккал)" )
+    proteins      = m.FloatField( default = 0, verbose_name = "Белки (г)" )
+    fats          = m.FloatField( default = 0, verbose_name = "Жиры (г)" )
+    carbohydrates = m.FloatField( default = 0, verbose_name = "Углеводы (г)" )
+    is_available  = m.BooleanField( default = True, verbose_name = "Доступна для заказа" )
+    created_at    = m.DateTimeField( auto_now_add = True, verbose_name = "Дата создания" )
+    updated_at    = m.DateTimeField( auto_now = True, verbose_name = "Дата обновления" )
 
     def save( self, *args, **kwargs ):
         if not self.slug:
@@ -57,12 +57,12 @@ class Shaurma( models.Model ):
         ordering = [ 'name' ]
 
 
-class ShaurmaCategory( models.Model ):
-    name        = models.CharField( max_length = 60, verbose_name = 'Название' )
-    description = models.TextField( max_length = 200, verbose_name = 'Описание' )
-    order       = models.PositiveSmallIntegerField( default = 0, verbose_name = "Порядок сортировки" )
-    created_at  = models.DateTimeField( auto_now_add = True, verbose_name = "Дата создания" )
-    updated_at  = models.DateTimeField( auto_now = True, verbose_name = "Дата обновления" )
+class ShaurmaCategory( m.Model ):
+    name        = m.CharField( max_length = 60, verbose_name = 'Название' )
+    description = m.TextField( max_length = 200, verbose_name = 'Описание' )
+    order       = m.PositiveSmallIntegerField( default = 0, verbose_name = "Порядок сортировки" )
+    created_at  = m.DateTimeField( auto_now_add = True, verbose_name = "Дата создания" )
+    updated_at  = m.DateTimeField( auto_now = True, verbose_name = "Дата обновления" )
 
     def __str__(self):
         return f'{self.name}'
@@ -73,15 +73,15 @@ class ShaurmaCategory( models.Model ):
         ordering = [ 'order', 'name' ]
 
 
-class Location( models.Model ):
-    address     = models.CharField( max_length = 60,  verbose_name = 'Адрес' )
-    description = models.TextField( max_length = 600, verbose_name = 'Описание' )
-    picture     = models.ImageField( upload_to = 'locations', verbose_name = 'Изображение' )
-    name        = models.CharField( max_length = 60, verbose_name = 'Название' )
-    contacts    = models.CharField( max_length = 18, verbose_name = 'Номер' )
-    city        = models.CharField( max_length = 30, verbose_name = 'Город' )
-    open_hours  = models.TimeField( verbose_name = 'Начало' )
-    close_hours = models.TimeField( verbose_name = 'Конец' )
+class Location( m.Model ):
+    address     = m.CharField( max_length = 60,  verbose_name = 'Адрес' )
+    description = m.TextField( max_length = 600, verbose_name = 'Описание' )
+    picture     = m.ImageField( upload_to = 'locations', verbose_name = 'Изображение' )
+    name        = m.CharField( max_length = 60, verbose_name = 'Название' )
+    contacts    = m.CharField( max_length = 18, verbose_name = 'Номер' )
+    city        = m.CharField( max_length = 30, verbose_name = 'Город' )
+    open_hours  = m.TimeField( verbose_name = 'Начало' )
+    close_hours = m.TimeField( verbose_name = 'Конец' )
     
     def __str__(self):
         return f'{self.name}'
@@ -116,16 +116,16 @@ class UserManager( BaseUserManager ):
 
 
 class User( AbstractBaseUser, PermissionsMixin ):
-    username     = models.CharField( max_length = 60, unique = True, verbose_name = 'Никнейм' )
-    picture      = models.ImageField( upload_to = 'user_images', verbose_name = 'Изображение' )
-    email        = models.EmailField( max_length = 80, verbose_name = 'Email' )
-    number       = models.CharField( max_length = 17, verbose_name = 'Номер' )
-    last_address = models.CharField( max_length = 200, verbose_name = 'Адрес последней доставки' )
-    reg_date     = models.DateTimeField( auto_now_add = True, verbose_name = 'время регистрации' )
+    username     = m.CharField( max_length = 60, unique = True, verbose_name = 'Никнейм' )
+    picture      = m.ImageField( upload_to = 'user_images', verbose_name = 'Изображение' )
+    email        = m.EmailField( max_length = 80, verbose_name = 'Email' )
+    number       = m.CharField( max_length = 17, verbose_name = 'Номер' )
+    last_address = m.CharField( max_length = 200, verbose_name = 'Адрес последней доставки' )
+    reg_date     = m.DateTimeField( auto_now_add = True, verbose_name = 'время регистрации' )
 
-    is_open   = models.BooleanField( default = True, verbose_name = 'Открытый профиль?' )
-    is_active = models.BooleanField( default = True, verbose_name = 'Профиль активен?' )
-    is_staff  = models.BooleanField( default = False, verbose_name = 'Это сотрудник сайта?' )
+    is_open   = m.BooleanField( default = True, verbose_name = 'Открытый профиль?' )
+    is_active = m.BooleanField( default = True, verbose_name = 'Профиль активен?' )
+    is_staff  = m.BooleanField( default = False, verbose_name = 'Это сотрудник сайта?' )
 
     objects = UserManager()
 
@@ -142,10 +142,10 @@ class User( AbstractBaseUser, PermissionsMixin ):
         ordering = [ 'username' ]
 
 
-class Order( models.Model ):
-    user    = models.ForeignKey( 'User', on_delete = models.CASCADE, verbose_name = 'Пользователь' )
-    shaurma = models.ForeignKey( 'Shaurma', on_delete = models.CASCADE, verbose_name = 'Шаурма' )
-    date    = models.DateTimeField( auto_now_add = True )
+class Order( m.Model ):
+    user    = m.ForeignKey( 'User', on_delete = m.CASCADE, verbose_name = 'Пользователь' )
+    shaurma = m.ForeignKey( 'Shaurma', on_delete = m.CASCADE, verbose_name = 'Шаурма' )
+    date    = m.DateTimeField( auto_now_add = True )
 
     def __str__(self):
         return f'Заказ {self.shaurma.name} от {self.user.username}'
@@ -154,19 +154,19 @@ class Order( models.Model ):
         verbose_name = 'заказ'
         verbose_name_plural = 'заказы'
 
-class Cart( models.Model ):
-    user = models.ForeignKey( 'User', on_delete = models.CASCADE, verbose_name='Пользователь' )
-    item = models.ForeignKey( 'Shaurma', on_delete = models.CASCADE, verbose_name = 'Шаурма' )
-    quanity = models.PositiveSmallIntegerField( 'Quanity', default=1 )
+class Cart( m.Model ):
+    user = m.ForeignKey( 'User', on_delete = m.CASCADE, verbose_name='Пользователь' )
+    item = m.ForeignKey( 'Shaurma', on_delete = m.CASCADE, verbose_name = 'Шаурма' )
+    quanity = m.PositiveSmallIntegerField( 'Quanity', default=1 )
 
     class Meta:
         verbose_name = 'корзина'
         verbose_name_plural = 'корзины'
 
 
-class Achievement( models.Model ):
-    name    = models.CharField( max_length = 60, verbose_name = 'Название' )
-    picture = models.ImageField( upload_to = 'achievement_image', verbose_name = 'Изображение' )
+class Achievement( m.Model ):
+    name    = m.CharField( max_length = 60, verbose_name = 'Название' )
+    picture = m.ImageField( upload_to = 'achievement_image', verbose_name = 'Изображение' )
 
     def __str__(self):
         return f'{self.name}'
@@ -177,10 +177,10 @@ class Achievement( models.Model ):
         ordering = [ 'name' ]
 
 
-class UserAchievement( models.Model ):
-    user        = models.ForeignKey( 'User', on_delete = models.CASCADE, verbose_name = 'Пользователь' )
-    achievement = models.ForeignKey( 'Achievement', on_delete = models.CASCADE, verbose_name = 'Достижение' )
-    get_date    = models.DateTimeField( auto_now_add = True, verbose_name = 'Время получения' )
+class UserAchievement( m.Model ):
+    user        = m.ForeignKey( 'User', on_delete = m.CASCADE, verbose_name = 'Пользователь' )
+    achievement = m.ForeignKey( 'Achievement', on_delete = m.CASCADE, verbose_name = 'Достижение' )
+    get_date    = m.DateTimeField( auto_now_add = True, verbose_name = 'Время получения' )
 
     def __str__(self):
         return f'{self.user.username} - {self.achievement.name}'
@@ -191,17 +191,17 @@ class UserAchievement( models.Model ):
         ordering = [ 'get_date' ]
 
 
-class Stock( models.Model ):
-    slug        = models.SlugField( max_length = 70, blank = True, verbose_name = "URL-адрес" )
-    name        = models.CharField( max_length = 60, verbose_name = 'Название' )
-    short_text  = models.CharField( max_length = 150, blank = True, verbose_name = 'Краткое описание' )
-    description = models.TextField( max_length = 1000, blank = True, verbose_name = 'Описание' )
-    condition   = models.TextField( max_length = 1000, blank = True, verbose_name = 'Условия акции' )
-    image       = models.ImageField( upload_to = 'stocks', verbose_name = 'Изображение' )
-    discount    = models.SmallIntegerField( verbose_name = 'Скидка в %' )
-    categories  = models.ManyToManyField( 'ShaurmaCategory', blank = True, verbose_name = 'Категория' )
-    date_start  = models.DateField( verbose_name = 'Старт' )
-    date_end    = models.DateField( verbose_name = 'Завершение' )
+class Stock( m.Model ):
+    slug        = m.SlugField( max_length = 70, blank = True, verbose_name = "URL-адрес" )
+    name        = m.CharField( max_length = 60, verbose_name = 'Название' )
+    short_text  = m.CharField( max_length = 150, blank = True, verbose_name = 'Краткое описание' )
+    description = m.TextField( max_length = 1000, blank = True, verbose_name = 'Описание' )
+    condition   = m.TextField( max_length = 1000, blank = True, verbose_name = 'Условия акции' )
+    image       = m.ImageField( upload_to = 'stocks', verbose_name = 'Изображение' )
+    discount    = m.SmallIntegerField( verbose_name = 'Скидка в %' )
+    categories  = m.ManyToManyField( 'ShaurmaCategory', blank = True, verbose_name = 'Категория' )
+    date_start  = m.DateField( verbose_name = 'Старт' )
+    date_end    = m.DateField( verbose_name = 'Завершение' )
 
     def save( self, *args, **kwargs ):
         if not self.slug:
@@ -217,13 +217,13 @@ class Stock( models.Model ):
         ordering = [ 'name' ]
 
 
-class Promocode( models.Model ):
-    code_name = models.CharField( max_length = 20, unique = True, verbose_name = 'Промокод' )
-    code_uuid = models.UUIDField( default=uuid.uuid4, editable=False, verbose_name = 'Промокод UUID' )
-    duration  = models.SmallIntegerField( default = 7, verbose_name = 'Время жизни ( в днях )' )
-    discount  = models.SmallIntegerField( default = 5, verbose_name = 'Скидка в %' )
-    date_add  = models.DateField( verbose_name = 'Дата создания' )
-    date_end  = models.DateField( null = True, blank = True, editable=False, verbose_name = 'Дата конца' )
+class Promocode( m.Model ):
+    code_name = m.CharField( max_length = 20, unique = True, verbose_name = 'Промокод' )
+    code_uuid = m.UUIDField( default=uuid.uuid4, editable=False, verbose_name = 'Промокод UUID' )
+    duration  = m.SmallIntegerField( default = 7, verbose_name = 'Время жизни ( в днях )' )
+    discount  = m.SmallIntegerField( default = 5, verbose_name = 'Скидка в %' )
+    date_add  = m.DateField( verbose_name = 'Дата создания' )
+    date_end  = m.DateField( null = True, blank = True, editable=False, verbose_name = 'Дата конца' )
 
     def save( self, *args, **kwargs ):
         if not self.id:
